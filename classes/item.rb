dcb3ef110ced244f0509fc014bb68a47fbfba784
Wali Muhammad <luckywali0986@gmail.com>
2023-03-28 01:19:05 +0500

    fix number by adding _ between

--- classes/item.rb
+++ classes/item.rb
@@ -2,7 +2,7 @@ class Item
   attr_reader :id, :genre, :author, :source, :label, :publish_date, :archived
 
   def initialize(publish_date)
-    @id = rand(1..10000)
+    @id = rand(1..100_00)
     @genre = nil
     @author = nil
     @source = nil
