f492255771852eac1ab10c0ef8aa7984a0d279e0
Wali Muhammad <luckywali0986@gmail.com>
2023-03-27 22:20:31 +0500

    add console app UI also change its permission to executable

---
+++ main.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require_relative './show_menu'
+require_relative './process_options'
+
+
+def main
+  loop do
+    show_menu
+    user_input = gets.chomp.upcase
+    puts
+    if user_input == 'D'
+      puts "\e[31mThank you for using this app! ♥️ \e[0m"
+      exit
+    end
+    process_options(user_input)
+  end
+end
+main
