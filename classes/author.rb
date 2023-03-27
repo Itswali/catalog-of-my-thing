bad2b7f88a82d7df75faadc3251b9ef0d3b9790d
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-27 13:53:53 -0600

    class author add

--- classes/author.rb
+++ classes/author.rb
@@ -2,13 +2,13 @@ require 'securerandom'
 require_relative 'item'
 
 class Author
-    def initialize (id = securerandom.uuid, first_name, last_name)
-        @id = id
-        @firs_name = firs_name
-        @last_name = last_name
-        @item = item []
-    end
+  def initialize(_first_name, last_name, id = securerandom.uuid)
+    @id = id
+    @firs_name = firs_name
+    @last_name = last_name
+    @item = item []
+  end
 
-    def add_item
-    end
+  #   def add_item
+  #   end
 end
