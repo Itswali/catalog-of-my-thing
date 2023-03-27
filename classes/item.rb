7021ca6dd36e7974686921da6b4d9ef69976ddfa
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-27 13:55:07 -0600

    item classe

--- classes/item.rb
+++ classes/item.rb
@@ -5,23 +5,24 @@ require_relative 'genre'
 require_relative 'author'
 
 class Item
-    def initialize (id = securerandom.uuid, genre, author, source, label, publish, archived, false)
-        @id = id
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        @publish = publish
-        @archived = false 
-    end
+  attr_accessor :genre, :author, :label, :publish_date, :archived
 
-    def can_be_archived?
-        return false unless publish < '2013-01-01'
-        true
-    end
+  def initialize(genre, author, label, publish_date = date, archived: false)
+    @id = SecureRandom.uuid
+    @genre = genre
+    @author = author
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
 
-    def move_to_archive
-        if  can_be_archived?
-            archived = true
-    end
+  def can_be_archived?
+    return false unless publish < '2013-01-01'
+
+    true
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 end
