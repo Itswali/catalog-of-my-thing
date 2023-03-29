f6bbdb03ff33b8d2e431cf6c70df31b5cf493830
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-29 10:12:35 -0600

    game modify

--- classes/game.rb
+++ classes/game.rb
@@ -4,10 +4,10 @@ require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(genre, author, label, publish_date, _last_played_at)
+  def initialize(genre, author, label, publish_date, last_played_at)
     super genre, author, label, publish_date
-    @multiplayer = multiplayer
-    @last_played_at = date
+    @multiplayer = false
+    @last_played_at = last_played_at
   end
   # def can_be_archived?
   # end
