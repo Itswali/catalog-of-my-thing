7a0060fb0b33fba6f4a65fc256cc34dd250d5cf2
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-29 15:39:22 -0600

    Book refactor

--- classes/book.rb
+++ classes/book.rb
@@ -3,13 +3,17 @@ require_relative 'item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state, publish_date)
-    super(publish_date)
+  def initialize(genre, author, label, publish_date, publisher)
+    super(genre, author, label, publish_date)
     @publisher = publisher
-    @cover_state = cover_state
+    @cover_state = 'bad'
   end
 
   def can_be_archived?
     super || cover_state == 'bad'
+  end
+
+  def self.list_all_books
+    ObjectSpace.each_object(self).to_a
   end
 end
