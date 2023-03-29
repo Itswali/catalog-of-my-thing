44cbcb262a1017f7dfee62f34034182bb8cf9ec0
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-29 15:39:00 -0600

    joseph part refactor

--- classes/author.rb
+++ classes/author.rb
@@ -1,14 +1,25 @@
-require 'securerandom'
-require_relative 'item'
+# class Author
 
-class Author
-  def initialize(_first_name, last_name, id = securerandom.uuid)
-    @id = id
-    @firs_name = firs_name
-    @last_name = last_name
-    @item = item []
-  end
+#   attr_accessor :first_name, :last_name, :id, :items
 
-  #   def add_item
-  #   end
-end
+#   def initialize(first_name, last_name)
+#     @id = rand(1..100_00)
+#     @first_name = first_name
+#     @last_name = last_name
+#     @items = []
+#   end
+
+#   def add_item(item)
+#     @items << item
+#     item.author = self
+#   end
+
+#   def self.list_all_authors
+#     ObjectSpace.each_object(self).to_a
+#   end
+
+#   def self.create_author(first_name, last_name)
+#     author_obj = list_all_authors.find {|author| author.first_name == first_name && author.last_name == last_name}
+#     author_obj.nil? ? new(first_name, last_name) : author_obj
+#   end
+# end
--- classes/game.rb
+++ classes/game.rb
@@ -1,14 +1,19 @@
-require 'securerandom'
-require_relative 'item'
+# require_relative 'item'
 
-class Game < Item
-  attr_accessor :multiplayer, :last_played_at
+# class Game < Item
+#   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(genre, author, label, publish_date, last_played_at)
-    super genre, author, label, publish_date
-    @multiplayer = false
-    @last_played_at = last_played_at
-  end
-  # def can_be_archived?
-  # end
-end
+#   def initialize(genre, author, label, publish_date, last_played_at)
+#     super genre, author, label, publish_date
+#     @multiplayer = false
+#     @last_played_at = last_played_at
+#   end
+
+#   def can_be_archived?
+#     super && ((Date.today.year - @last_played_at.year) > 2)
+#   end
+
+#   def self.list_all_games
+#     ObjectSpace.each_object(self).to_a
+#   end
+# end
