47e2d76eae290d351c9765507df323e1b0b4932d
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-29 15:39:29 -0600

    create genre class

--- classes/genre.rb
+++ classes/genre.rb
@@ -8,11 +8,16 @@ class Genre
   end
 
   def add_item(item)
-    @item << item  
+    @item << item
     item.genre = self
   end
 
   def self.list_all_genre
     ObjectSpace.each_object(self).to_a
+  end
+
+  def self.create_genre(name)
+    genre_obj = list_all_genre.find { |genre| genre.name == name }
+    genre_obj.nil? ? new(name) : genre_obj
   end
 end
