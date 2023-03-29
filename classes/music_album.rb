d0e7eac980dae946b92dabb4d9632ed730c2e444
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-29 15:39:54 -0600

    music album class

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,14 +1,18 @@
 require_relative 'item'
 
-class Musicalbum < Item
+class Music_album < Item
   attr_accessor :on_spotify
 
-  def initialize(genre: nil, author: nil, label: nil, publish_date: nil, on_spotify: false)
-    super(genre: genre, author: author, label: label, publish_date: publish_date)
+  def initialize(genre, author, label, publish_date, on_spotify: false)
+    super(genre, author, label, publish_date)
     @on_spotify = on_spotify
   end
 
   def can_be_archived?
     super && @on_spotify
+  end
+
+  def self.list_all_music_album
+    ObjectSpace.each_object(self).to_a
   end
 end
