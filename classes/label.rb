1a223b74a8d4b237194a12a138a8b8b8824afb56
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-29 15:39:43 -0600

    refactor label

--- classes/label.rb
+++ classes/label.rb
@@ -1,8 +1,7 @@
 class Label
-  attr_accessor :title, :color
-  attr_reader :id, :items
+  attr_accessor :title, :color, :id, :items
 
-  def initialize(title, color, _items)
+  def initialize(title, color)
     @id = rand(1..100_00)
     @title = title
     @color = color
@@ -12,5 +11,14 @@ class Label
   def add_item(item)
     @items << item
     item.label = self
+  end
+
+  def self.list_all_label
+    ObjectSpace.each_object(self).to_a
+  end
+
+  def self.create_label(title, color)
+    label_obj = list_all_label.find { |label| label.title == title && label.color == color }
+    label_obj.nil? ? new(title, color) : label_obj
   end
 end
