3a562b26d2fa6c5ccb797e3e1b71dd28234b6365
ONJoseph <on.josephinfo@gmail.com>
2023-03-30 15:49:03 +0100

    Add author class

--- classes/author.rb
+++ classes/author.rb
@@ -1,14 +1,24 @@
-require 'securerandom'
-require_relative 'item'
-
 class Author
-  def initialize(_first_name, last_name, id = securerandom.uuid)
-    @id = id
-    @firs_name = firs_name
+  attr_accessor :first_name, :last_name, :id, :items
+
+  def initialize(first_name, last_name)
+    @id = rand(1..100_00)
+    @first_name = first_name
     @last_name = last_name
-    @item = item []
+    @items = []
   end
 
-  #   def add_item
-  #   end
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
+
+  def self.list_all_authors
+    ObjectSpace.each_object(self).to_a
+  end
+
+  def self.create_author(first_name, last_name)
+    author_obj = list_all_authors.find { |author| author.first_name == first_name && author.last_name == last_name }
+    author_obj.nil? ? new(first_name, last_name) : author_obj
+  end
 end
