67a85a5148f97a9d1f54aea0bd3e1ff3c9f75fe6
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-30 10:50:13 -0600

    genre save data json

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,8 +1,8 @@
 class Genre
   attr_accessor :id, :name, :items
 
-  def initialize(name)
-    @id = rand(1..100_00)
+  def initialize(name, id = rand(1..100_00))
+    @id = id
     @name = name
     @items = []
   end
@@ -19,5 +19,20 @@ class Genre
   def self.create_genre(name)
     genre_obj = list_all_genre.find { |genre| genre.name == name }
     genre_obj.nil? ? new(name) : genre_obj
+  end
+
+  def to_json(*_option)
+    {
+      id: @id,
+      name: @name
+    }.to_json
+  end
+
+  def self.from_hash(genre)
+    new(genre['name'], genre['id'])
+  end
+
+  def self.from_hash_array(array)
+    array.each { |genre| from_hash(genre) }
   end
 end
