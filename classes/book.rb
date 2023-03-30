c748808c2b2a484b0289dc4def66e609fa319a71
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-30 10:51:06 -0600

    save data book

--- classes/book.rb
+++ classes/book.rb
@@ -16,4 +16,25 @@ class Book < Item
   def self.list_all_books
     ObjectSpace.each_object(self).to_a
   end
+
+  def to_json(*_option)
+    {
+      genre_id: @genre.id,
+      author_id: @author.id,
+      label_id: @label.id,
+      publish_date: @publish_date,
+      publisher: @publisher
+    }.to_json
+  end
+
+  def self.from_hash(book)
+    genre_obj = Genre.list_all_genre.find { |genre| genre.id == book['genre_id'] }
+    label_obj = Label.list_all_label.find { |label| label.id == book['label_id'] }
+    author_obj = Author.list_all_authors.find { |author| author.id == book['author_id'] }
+    new(genre_obj, author_obj, label_obj, book['publish_date'], book['publisher'])
+  end
+
+  def self.from_hash_array(array)
+    array.each { |book| from_hash(book) }
+  end
 end
