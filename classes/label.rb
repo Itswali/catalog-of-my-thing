8202ad8cd5512a51913d5c8bad64ae2d1931d365
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-30 10:51:21 -0600

    save data label

--- classes/label.rb
+++ classes/label.rb
@@ -1,8 +1,8 @@
 class Label
   attr_accessor :title, :color, :id, :items
 
-  def initialize(title, color)
-    @id = rand(1..100_00)
+  def initialize(title, color, id = rand(1..100_00))
+    @id = id
     @title = title
     @color = color
     @items = []
@@ -20,5 +20,21 @@ class Label
   def self.create_label(title, color)
     label_obj = list_all_label.find { |label| label.title == title && label.color == color }
     label_obj.nil? ? new(title, color) : label_obj
+  end
+
+  def to_json(*_option)
+    {
+      id: @id,
+      title: @title,
+      color: @color
+    }.to_json
+  end
+
+  def self.from_hash(label)
+    new(label['title'], label['color'], label['id'])
+  end
+
+  def self.from_hash_array(array)
+    array.each { |label| from_hash(label) }
   end
 end
