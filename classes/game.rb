f64652812d6b14ae5363f953f1c2b67df4b99c9d
ONJoseph <on.josephinfo@gmail.com>
2023-03-30 15:49:20 +0100

    Add game class

--- classes/game.rb
+++ classes/game.rb
@@ -1,14 +1,18 @@
-require 'securerandom'
 require_relative 'item'
-
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(genre, author, label, publish_date, _last_played_at)
+  def initialize(genre, author, label, publish_date, last_played_at)
     super genre, author, label, publish_date
-    @multiplayer = multiplayer
-    @last_played_at = date
+    @multiplayer = false
+    @last_played_at = last_played_at
   end
-  # def can_be_archived?
-  # end
+
+  def can_be_archived?
+    super && ((Date.today.year - @last_played_at.year) > 2)
+  end
+
+  def self.list_all_games
+    ObjectSpace.each_object(self).to_a
+  end
 end
